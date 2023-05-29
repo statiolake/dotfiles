d6f9065df1f86e1b7290a6c1af07f30021290957
statiolake <statiolake@gmail.com>
2023-05-29 23:08:47 +0900

    feat(neovim): use nvim-cmp

--- neovim/rtp/lua/rc/part/config.lua
+++ neovim/rtp/lua/rc/part/config.lua
@@ -5,8 +5,8 @@ set_global_config {
   ['editor.formatOnSave'] = true,
   ['editor.ide.framework'] = 'builtin', -- [builtin, coc]
   ['editor.ide.coc.selector'] = 'coc', -- [telescope, coc, fzf]
-  ['editor.ide.builtin.completion'] = 'ddc', -- [ddc, nvim-cmp]
-  ['editor.ide.builtin.useFloatPum'] = false,
+  ['editor.ide.builtin.completion'] = 'nvim-cmp', -- [ddc, nvim-cmp]
+  ['editor.ide.builtin.useFloatPum'] = true,
   ['editor.ide.builtin.useSnip'] = 'triggerOnly', -- [always, triggerOnly, never]
   ['editor.ide.builtin.selector'] = 'telescope', -- [telescope, fzf, ddu]
   ['editor.useTreesitter'] = true,
