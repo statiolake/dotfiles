a4f19299759034d0c3e6d2c99213ab5e3d39ecdb
statiolake <statiolake@gmail.com>
2023-06-04 00:29:29 +0900

    feat(powershell): notify current directory to terminal

--- WindowsPowerShell/basic.ps1
+++ WindowsPowerShell/basic.ps1
@@ -21,12 +21,20 @@ function Prompt () {
     )
     $prompt_char = $is_admin ? '#' : '>'
 
+    $location = $ExecutionContext.SessionState.Path.CurrentLocation
+
     Write-Host $time_string -NoNewline -ForegroundColor White
     Write-Host " " -NoNewline
     Write-Host ([String]::Format("{0}@{1}", $env:USERNAME, $env:COMPUTERNAME)) -NoNewline -ForegroundColor Green
     Write-Host ":" -NoNewline
-    Write-Host (Get-Location) -NoNewline -ForegroundColor Blue
+    Write-Host $location -NoNewline -ForegroundColor Blue
     Write-Host
 
-    [String]::Format("{0}{1} ", $face, $prompt_char)
+    $rest = [String]::Format("{0}{1} ", $face, $prompt_char)
+    if ($location.Provider.Name -eq "FileSystem") {
+        $notify_location = "$([char]27)]9;9;`"$($location.ProviderPath)`"$([char]27)\"
+        $rest += $notify_location
+    }
+
+    return $rest
 }
