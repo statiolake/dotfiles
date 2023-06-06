c2e2b6423d6deb92a3b4d672c698c795eda4a64a
statiolake <statiolake@gmail.com>
2023-06-06 17:27:05 +0900

    refactor(powershell): use camel case

--- WindowsPowerShell/basic.ps1
+++ WindowsPowerShell/basic.ps1
@@ -1,40 +1,39 @@
 ﻿# Set prompt
 function Prompt () {
     # Save last status before running bunch of commands to construct prompt...
-    $last_status = $?
+    $lastStatus = $?
 
     $location = Get-Location
     $now = Get-Date
-    $time_string = [String]::Format(
+    $timeString = [String]::Format(
         "{0:00}:{1:00}:{2:00}",
         $now.Hour,
         $now.Minute,
         $now.Second
     )
-    $face = ($last_status ? "('-')/" : "('-';)")
+    $face = ($lastStatus ? "('-')/" : "('-';)")
 
-    $security_identity = (
+    $securityIdentity = (
         [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
     )
-    $is_admin = $security_identity.IsInRole(
+    $isAdmin = $securityIdentity.IsInRole(
         [System.Security.Principal.WindowsBuiltInRole] "Administrator"
     )
-    $prompt_char = $is_admin ? '#' : '>'
+    $promptChar = $isAdmin ? '#' : '>'
 
     $location = $ExecutionContext.SessionState.Path.CurrentLocation
 
-    Write-Host $time_string -NoNewline -ForegroundColor White
+    Write-Host $timeString -NoNewline -ForegroundColor White
     Write-Host " " -NoNewline
     Write-Host ([String]::Format("{0}@{1}", $env:USERNAME, $env:COMPUTERNAME)) -NoNewline -ForegroundColor Green
     Write-Host ":" -NoNewline
     Write-Host $location -NoNewline -ForegroundColor Blue
     Write-Host
 
-    $rest = [String]::Format("{0}{1} ", $face, $prompt_char)
     if ($location.Provider.Name -eq "FileSystem") {
-        $notify_location = "$([char]27)]9;9;`"$($location.ProviderPath)`"$([char]27)\"
-        $rest += $notify_location
+        $notifyLocation = "$([char]27)]9;9;`"$($location.ProviderPath)`"$([char]27)\"
+        Write-Host $notifyLocation -NoNewline
     }
 
-    return $rest
+    return [String]::Format("{0}{1} ", $face, $promptChar)
 }
