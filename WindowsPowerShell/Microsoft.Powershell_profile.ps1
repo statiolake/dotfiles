dd3f828e43dad6563c4ad17ab92a974743f6774a
statiolake <statiolake@gmail.com>
2023-06-17 02:48:29 +0900

    fix(powershell): ignore profile in old (bundled) powershell

--- WindowsPowerShell/Microsoft.Powershell_profile.ps1
+++ WindowsPowerShell/Microsoft.Powershell_profile.ps1
@@ -1,32 +1,35 @@
-﻿Import-Module PSReadLine
-Set-PSReadlineOption -EditMode Emacs
-
-# fix Ctrl+l
-Set-PSReadlineKeyHandler -Chord "Ctrl+l" -ScriptBlock {
-  Clear-Host
-  [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt($null, 0)
-}
-
-# default editor
-if ( -not $env:EDITOR )
+﻿if ($PSVersionTable.PSVersion.Major -ge 7)
 {
-  $env:EDITOR = "v.exe"
-}
+  Import-Module PSReadLine
+  Set-PSReadlineOption -EditMode Emacs
 
-$confdir = (Get-Item $profile).DirectoryName
+  # fix Ctrl+l
+  Set-PSReadlineKeyHandler -Chord "Ctrl+l" -ScriptBlock {
+    Clear-Host
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt($null, 0)
+  }
 
-# example: ~\Documents\WindowsPowerShell\basic.ps1
-. "$confdir\basic.ps1"
+  # default editor
+  if ( -not $env:EDITOR )
+  {
+    $env:EDITOR = "v.exe"
+  }
 
-# example: ~\Documents\WindowsPowerShell\function.ps1
-. "$confdir\function.ps1"
+  $confdir = (Get-Item $profile).DirectoryName
 
-# example: ~\Documents\WindowsPowerShell\alias.ps1
-. "$confdir\alias.ps1"
+  # example: ~\Documents\WindowsPowerShell\basic.ps1
+  . "$confdir\basic.ps1"
 
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile))
-{
-  Import-Module "$ChocolateyProfile"
+  # example: ~\Documents\WindowsPowerShell\function.ps1
+  . "$confdir\function.ps1"
+
+  # example: ~\Documents\WindowsPowerShell\alias.ps1
+  . "$confdir\alias.ps1"
+
+  # Chocolatey profile
+  $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+  if (Test-Path($ChocolateyProfile))
+  {
+    Import-Module "$ChocolateyProfile"
+  }
 }
